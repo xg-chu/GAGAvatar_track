53715277342e58093626c2e725bbf1937daf7971
Xuangeng Chu <xg.chu@outlook.com>
2024-09-29 03:02:53 +0000

    modify track_lmdb to noshareid[default]

--- track_lmdb.py
+++ track_lmdb.py
@@ -14,7 +14,7 @@ class Tracker:
         self._device = device
         self.tracker = CoreEngine(focal_length=focal_length, device=device)
 
-    def track_lmdb(self, lmdb_path, dir_path=None, no_shareid=False):
+    def track_lmdb(self, lmdb_path, dir_path=None):
         # build name
         data_name = os.path.basename(lmdb_path[:-1] if lmdb_path.endswith('/') else lmdb_path)
         output_path = os.path.join('outputs', dir_path) if dir_path else f'outputs/{data_name}'
@@ -26,20 +26,20 @@ class Tracker:
         base_results = self.tracker.track_base(lmdb_engine, output_path)
         print('Track with flame/bbox done!')
         print('Track optim...')
-        if not no_shareid:
-            print('Share the shapecode of each video before optimization...')
-            video_names = list(set(['_'.join(frame_name.split('_')[:-1]) for frame_name in base_results.keys()]))
-            video_frames = {video_name: [] for video_name in video_names}
-            for frame_name in base_results.keys():
-                video_name = '_'.join(frame_name.split('_')[:-1])
-                video_frames[video_name].append(frame_name)
-            for video_name in video_names:
-                shapecode = np.stack([
-                        base_results[frame_name]['emica_results']['shapecode'] for frame_name in video_frames[video_name]
-                    ], axis=0
-                ).mean(axis=0)
-                for frame_name in video_frames[video_name]:
-                    base_results[frame_name]['emica_results']['shapecode'] = shapecode
+        # if shareid:
+        #     print('Share the shapecode of each video before optimization...')
+        #     video_names = list(set(['_'.join(frame_name.split('_')[:-1]) for frame_name in base_results.keys()]))
+        #     video_frames = {video_name: [] for video_name in video_names}
+        #     for frame_name in base_results.keys():
+        #         video_name = '_'.join(frame_name.split('_')[:-1])
+        #         video_frames[video_name].append(frame_name)
+        #     for video_name in video_names:
+        #         shapecode = np.stack([
+        #                 base_results[frame_name]['emica_results']['shapecode'] for frame_name in video_frames[video_name]
+        #             ], axis=0
+        #         ).mean(axis=0)
+        #         for frame_name in video_frames[video_name]:
+        #             base_results[frame_name]['emica_results']['shapecode'] = shapecode
         optim_results = self.tracker.track_optim(base_results, output_path, lmdb_engine, share_id=False)
         print('Track optim done!')
         lmdb_engine.close()
@@ -54,10 +54,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--lmdb_path', '-l', required=True, type=str)
     parser.add_argument('--outdir_path', '-d', default='', type=str)
-    parser.add_argument('--no_shareid', action='store_true')
     parser.add_argument('--split_id', '-s', default=0, type=int)
     args = parser.parse_args()
     
     tracker = Tracker(focal_length=12.0, device='cuda')
-    tracker.track_lmdb(args.lmdb_path, dir_path=args.outdir_path, no_shareid=args.no_shareid)
+    tracker.track_lmdb(args.lmdb_path, dir_path=args.outdir_path)
    
